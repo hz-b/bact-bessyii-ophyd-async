1f3c15ce738b8e1ad7f8e7d8bb6dce8943b4795f
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-04-14 12:59:56 +0200

    [TASK] check functions for bluesky

--- tests/test_topup_engine_devs.py
+++ tests/test_topup_engine_devs.py
@@ -14,20 +14,22 @@ async def test_raw_engine_startup():
     await engine.connect()
     r = await engine.read()
     # 6 entries should be there
-    assert(len(r) == 6)
+    assert len(r) == 6
 
 
 @pytest.mark.asyncio
 async def test_system_startup():
     topup = TopUpSystem(
-        "TOPUPCC:",
-        name="topup",
-        target_current=25,
-        acceptable_loss=0.1
+        "TOPUPCC:", name="topup", target_current=25, acceptable_loss=0.1
     )
     await topup.connect()
     await topup.stage()
     r = await topup.read()
     # 6 entries should be there
-    assert(len(r) == 6)
+    assert len(r) == 6
+    r = await topup.describe()
+    # 6 entries should be there
+    assert len(r) == 6
     await topup.unstage()
+    assert callable(topup.frequency.set)
+    assert callable(topup.topup.set)
